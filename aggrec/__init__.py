1c1e2258c08ca5209d71a1c6954185b4c60e7a1e
Jakob Schlyter <jakob@kirei.se>
2024-11-07 15:26:47 +0100

    tweek

--- aggrec/__init__.py
+++ aggrec/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import version
 __version__ = version("aggrec")
 
 try:
-    from aggrec.buildinfo import __commit__, __timestamp__
+    from .buildinfo import __commit__, __timestamp__
 
     __verbose_version__ = f"{__version__} ({__commit__})"
 except ModuleNotFoundError:
