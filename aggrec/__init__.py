5a1234dae0bad58de581c682b47b2a2eb4ad89c7
Jakob Schlyter <jakob@kirei.se>
2025-05-01 18:10:15 +0200

    Version number fallback (#198)

--- aggrec/__init__.py
+++ aggrec/__init__.py
@@ -1,6 +1,9 @@
-from importlib.metadata import version
+from importlib.metadata import PackageNotFoundError, version
 
-__version__ = version("aggrec")
+try:
+    __version__ = version("aggrec")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
 try:
     from .buildinfo import __commit__, __timestamp__
