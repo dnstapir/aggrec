1c3aa6c8f7265cd2b06ebb62f997a3fb9c30da53
Jakob Schlyter <jakob@kirei.se>
2025-11-19 08:33:31 +0100

    Merge pipelines

--- aggrec/creators.py
+++ aggrec/creators.py
@@ -1,5 +1,4 @@
 import logging
-from collections import defaultdict
 
 from fastapi import APIRouter, Request
 
@@ -11,33 +10,22 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-GET_CREATORS_LAST_PIPELINE = [
+# Pipeline to get creators with their aggregate statistics
+GET_CREATORS_PIPELINE = [
     {
-        "$sort": {
-            "_id": -1,
-        }
+        "$sort": {"_id": -1},
     },
     {
         "$group": {
             "_id": "$creator",
             "last_id": {"$first": "$_id"},
-        }
-    },
-    {
-        "$sort": {
-            "_id": 1,
-        }
-    },
-]
-
-GET_CREATORS_COUNT_PIPELINE = [
-    {
-        "$group": {
-            "_id": "$creator",
             "aggregates_count": {"$sum": 1},
             "aggregates_total_size": {"$sum": "$content_length"},
         }
-    }
+    },
+    {
+        "$sort": {"_id": 1},
+    },
 ]
 
 
@@ -50,23 +38,15 @@ def get_creators(
 ) -> CreatorsResponse:
     """Get a list of all creators."""
 
-    count: dict[str, int] = defaultdict(int)
-    total_size: dict[str, int] = defaultdict(int)
-
-    for obj in AggregateMetadata.objects().aggregate(GET_CREATORS_COUNT_PIPELINE):
-        creator = obj["_id"]
-        count[creator] = obj["aggregates_count"]
-        total_size[creator] = obj["aggregates_total_size"]
-
     creators = [
         CreatorInformation(
             creator=obj["_id"],
             last_aggregate_id=str(obj["last_id"]),
             last_seen=obj["last_id"].generation_time,
-            aggregates_count=count[obj["_id"]],
-            aggregates_total_size=total_size[obj["_id"]],
+            aggregates_count=obj["aggregates_count"],
+            aggregates_total_size=obj["aggregates_total_size"],
         )
-        for obj in AggregateMetadata.objects().aggregate(GET_CREATORS_LAST_PIPELINE)
+        for obj in AggregateMetadata.objects().aggregate(GET_CREATORS_PIPELINE)
     ]
 
     return CreatorsResponse(creators=creators)
